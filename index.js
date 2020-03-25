851740e3cffd45ff6a569a1b61d80cb7a67b7dc8
Avaer Kazmer <a@modules.io>
2020-03-25 08:01:49 -0400

    Clean up index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const http = require('http');
 const express = require('express');
 
 const app = express();
-app.use('*', express.static(__dirname));
-const server = http.createServer(app);
-server.listen(3000);
+app.use(express.static(__dirname));
+
+http.createServer(app)
+  .listen(3000);
+
+console.log(`http://127.0.0.1:3000`);
