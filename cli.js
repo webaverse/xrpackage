2febd46889247cf995e76fbc28882fa1481a3adb
Avaer Kazmer <a@modules.io>
2020-03-26 17:03:28 -0400

    CLI debugging

--- cli.js
+++ cli.js
@@ -1,98 +1,105 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const yargs = require('yargs');
 const wbn = require('wbn');
 // const {XRPackage} = require('./xrpackage.js');
 
-require('yargs')
-  .command('build [input] [output]', 'build xrpackage .wbn from [input] and write to [output]', yargs => {
-    yargs
-      .positional('input', {
-        describe: 'input file to build',
-        // default: 5000
-      })
-      .positional('output', {
-        describe: 'output file to write',
-        // default: 5000
-      });
-  }, async argv => {
-    if (typeof argv.input !== 'string') {
-      argv.input = '-';
-    }
-    if (typeof argv.output !== 'string') {
-      argv.output = 'a.wbn';
-    }
+let handled = false;
 
-    const fileData = await (() => {
-      if (argv.input === '-') {
-        return new Promise((accept, reject) => {
-          const bs = [];
-          process.stdin.on('data', d => {
-            bs.push(d);
-          });
-          process.stdin.once('end', () => {
-            accept(Buffer.concat(bs));
-          });
-          process.stdin.once('error', reject);
+yargs.command('build [input] [output]', 'build xrpackage .wbn from [input] and write to [output]', yargs => {
+  yargs
+    .positional('input', {
+      describe: 'input file to build',
+      // default: 5000
+    })
+    .positional('output', {
+      describe: 'output file to write',
+      // default: 5000
+    });
+}, async argv => {
+  handled = true;
+
+  if (typeof argv.input !== 'string') {
+    argv.input = '-';
+  }
+  if (typeof argv.output !== 'string') {
+    argv.output = 'a.wbn';
+  }
+
+  const fileData = await (() => {
+    if (argv.input === '-') {
+      return new Promise((accept, reject) => {
+        const bs = [];
+        process.stdin.on('data', d => {
+          bs.push(d);
         });
-      } else {
-        return Promise.resolve(fs.readFileSync(argv.input));
-      }
-    })();
-
-    // console.log('got data', data.length);
-
-    let spatialType, mimeType;
-    if (/\.gltf$/.test(argv.input)) {
-      spatialType = 'gltf@0.0.1';
-      mimeType = 'model/gltf+json';
-    } else if (/\.glb$/.test(argv.input)) {
-      spatialType = 'gltf@0.0.1';
-      mimeType = 'application/octet-stream';
-    } else if (/\.vrm$/.test(argv.input)) {
-      spatialType = 'vrm@0.0.1';
-      mimeType = 'application/octet-stream';
-    } else if (/\.html$/.test(argv.input)) {
-      spatialType = 'webxr-site@0.0.1';
-      mimeType = 'text/html';
+        process.stdin.once('end', () => {
+          accept(Buffer.concat(bs));
+        });
+        process.stdin.once('error', reject);
+      });
     } else {
-      throw new Error(`unknown file type: ${argv.input}`);
+      return Promise.resolve(fs.readFileSync(argv.input));
     }
+  })();
 
-    const files = [
-      {
-        url: '/',
-        type: mimeType,
-        data: fileData,
-      },
-      {
-        url: '/manifest.json',
-        type: 'application/json',
-        data: JSON.stringify({
-          spatial_type: spatialType,
-        }, null, 2),
-      },
-    ];
+  // console.log('got data', data.length);
 
-    const primaryUrl = `https://xrpackage.org`;
-    const builder = (new wbn.BundleBuilder(primaryUrl + '/'))
-    for (let i = 0; i < files.length; i++) {
-      const file = files[i];
-      const {url, type, data} = file;
-      builder.addExchange(primaryUrl + url, 200, {
-        'Content-Type': type,
-      }, data);
-    }
-    const uint8Array = builder.createBundle();
-    // console.log('got bundle', uint8Array.byteLength);
+  let spatialType, mimeType;
+  if (/\.gltf$/.test(argv.input)) {
+    spatialType = 'gltf@0.0.1';
+    mimeType = 'model/gltf+json';
+  } else if (/\.glb$/.test(argv.input)) {
+    spatialType = 'gltf@0.0.1';
+    mimeType = 'application/octet-stream';
+  } else if (/\.vrm$/.test(argv.input)) {
+    spatialType = 'vrm@0.0.1';
+    mimeType = 'application/octet-stream';
+  } else if (argv.input === '-' || /\.html$/.test(argv.input)) {
+    spatialType = 'webxr-site@0.0.1';
+    mimeType = 'text/html';
+  } else {
+    throw new Error(`unknown file type: ${argv.input}`);
+  }
 
-    fs.writeFileSync(argv.output, uint8Array);
+  const files = [
+    {
+      url: '/',
+      type: mimeType,
+      data: fileData,
+    },
+    {
+      url: '/manifest.json',
+      type: 'application/json',
+      data: JSON.stringify({
+        spatial_type: spatialType,
+      }, null, 2),
+    },
+  ];
 
-    console.log(argv.output);
+  const primaryUrl = `https://xrpackage.org`;
+  const builder = (new wbn.BundleBuilder(primaryUrl + '/'))
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    const {url, type, data} = file;
+    builder.addExchange(primaryUrl + url, 200, {
+      'Content-Type': type,
+    }, data);
+  }
+  const uint8Array = builder.createBundle();
+  // console.log('got bundle', uint8Array.byteLength);
 
-    /* if (argv.verbose) console.info(`start server on :${argv.port}`)
-    serve(argv.port) */
-  }).argv;
+  fs.writeFileSync(argv.output, uint8Array);
+
+  console.log(argv.output);
+
+  /* if (argv.verbose) console.info(`start server on :${argv.port}`)
+  serve(argv.port) */
+}).argv;
+if (!handled) {
+  yargs.showHelp();
+}
   /* .option('verbose', {
     alias: 'v',
     type: 'boolean',
