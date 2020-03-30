793fe4f2697660c5bbe1bbbc9e96fee94c48cf57
Avaer Kazmer <a@modules.io>
2020-03-30 14:07:00 -0400

    Clean up storage options flow

--- popup.js
+++ popup.js
@@ -1,31 +1,37 @@
 window.addEventListener('load', e => {
 
 let options = localStorage.getItem('options');
-// console.log('got options', options);
-options = options ? JSON.parse(options) : {};
-const {browser, webxr, webvr} = options;
 
-if (browser) {
-  document.getElementById('browser').value = browser;
-}
-document.getElementById('webxr').checked = !!webxr;
-document.getElementById('webvr').checked = !!webvr;
+// Read it using the storage API
+chrome.storage.sync.get(['options'], function(items) {
+  let {options = {}} = items;
+  const {browser, webxr, webvr} = options;
 
-function _save() {
-  console.log('save', options);
-  localStorage.setItem('options', JSON.stringify(options));
-}
-document.getElementById('browser').addEventListener('change', () => {
-  options.browser = document.getElementById('browser').value;
-  _save();
-});
-document.getElementById('webxr').addEventListener('change', () => {
-  options.webxr = document.getElementById('webxr').checked;
-  _save();
-});
-document.getElementById('webvr').addEventListener('change', () => {
-  options.webvr = document.getElementById('webvr').checked;
-  _save();
+  if (browser) {
+    document.getElementById('browser').value = browser;
+  }
+  document.getElementById('webxr').checked = !!webxr;
+  document.getElementById('webvr').checked = !!webvr;
+
+  function _save() {
+    console.log('save', options);
+    chrome.storage.sync.set({options}, function() {
+      // nothing
+    });
+  }
+  document.getElementById('browser').addEventListener('change', () => {
+    options.browser = document.getElementById('browser').value;
+    _save();
+  });
+  document.getElementById('webxr').addEventListener('change', () => {
+    options.webxr = document.getElementById('webxr').checked;
+    _save();
+  });
+  document.getElementById('webvr').addEventListener('change', () => {
+    options.webvr = document.getElementById('webvr').checked;
+    _save();
+  });
+
 });
 
 });
