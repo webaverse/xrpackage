5894de6dd4f0f38fc1fc1fc104ba10089d598e89
Avaer Kazmer <a@modules.io>
2020-04-06 19:08:24 -0400

    Add inventory ls command

--- cli.js
+++ cli.js
@@ -408,6 +408,78 @@ yargs
     console.log(`${tokenHost}/${id}`);
     console.log(`https://${network}.opensea.io/assets/${contract._address}/${id}`);
   })
+  .command('ls', 'list wallet inventory', yargs => {
+    yargs
+      /* .positional('id', {
+        describe: 'id of package to install',
+        // default: 5000
+      }) */
+  }, async argv => {
+    handled = true;
+
+    const ksString = fs.readFileSync(path.join(os.homedir(), '.xrpackage'));
+
+    const mutableStdout = new Writable({
+      write: function(chunk, encoding, callback) {
+        if (!this.muted)
+          process.stdout.write(chunk, encoding);
+        callback();
+      }
+    });
+    mutableStdout.muted = false;
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: mutableStdout,
+      terminal: true
+    });
+
+    const passwordPromise = makePromise();
+    rl.question('password: ', password => {
+      rl.close();
+
+      passwordPromise.accept(password);
+    });
+    const password = await passwordPromise;
+
+    const ks = await _importKeyStore(ksString, password);
+
+    const contract = await getContract;
+    const owner = '0x' + ks.addresses[0];
+    const owners = Array(100);
+    const ids = Array(owners.length);
+    for (let i = 0; i < ids.length; i++) {
+      owners[i] = owner;
+      ids[i] = i+1;
+    }
+    const balances = await contract.methods.balanceOfBatch(owners, ids).call();
+    const ownedIds = balances.map((balance, id) => {
+      balance = parseInt(balance, 10);
+      if (balance > 0) {
+        return id;
+      } else {
+        return null;
+      }
+    }).filter(id => id !== null);
+    const objects = [];
+    for (let i = 0; i < ownedIds.length; i++) {
+      const id = ownedIds[i];
+      const metadataHash = await contract.methods.getMetadata(id, 'hash').call();
+      const metadata = await fetch(`${apiHost}/${metadataHash}`)
+        .then(res => res.json());
+      const {objectName, dataHash} = metadata;
+      objects.push({
+        id,
+        objectName,
+        dataHash,
+      });
+    }
+    for (let i = 0; i < ownedIds.length; i++) {
+      const object = objects[i];
+      console.log(object.id + ' ' + JSON.stringify(object.objectName) + ' ' + `${apiHost}/${object.dataHash}.wbn`);
+    }
+    /* const nonce = await contract.methods.getNonce().call();
+    console.log(nonce); */
+  })
   .command('count', 'get count of published packages', yargs => {
     yargs
       /* .positional('id', {
