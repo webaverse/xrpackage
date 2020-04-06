c0716dee70a199f22697c20aa8cd3446fd42ee35
Avaer Kazmer <a@modules.io>
2020-04-06 17:57:40 -0400

    Update tokens URL in CLI

--- cli.js
+++ cli.js
@@ -20,7 +20,7 @@ const express = require('express');
 const opn = require('opn');
 
 const apiHost = `https://ipfs.exokit.org/ipfs`;
-const tokenHost = `https://tokens.cryptopolys.com`;
+const tokenHost = `https://tokens.webaverse.com`;
 const network = 'rinkeby';
 const infuraApiKey = '4fb939301ec543a0969f3019d74f80c2';
 const rpcUrl = `https://${network}.infura.io/v3/${infuraApiKey}`;
