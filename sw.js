f662de202afef86ff3522f3b617acef5761b5c35
Avaer Kazmer <a@modules.io>
2020-05-05 00:12:43 -0400

    Fetch cleanup in sw.js

--- sw.js
+++ sw.js
@@ -37,17 +37,19 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
+  console.log('got fetch', event);
   const {
     clientId
   } = event;
   event.respondWith(
     clients.get(clientId)
     .then(client => {
+      const u = new URL(event.request.url);
+      const {pathname} = u;
       // console.log('got client', event.request.url, !!(client && client.frameType === 'nested'));
       if (client && client.frameType === 'nested') {
-        const u = new URL(event.request.url);
         // console.log('got client', u.pathname, client, event.request);
-        if (event.request.method === 'POST' && u.pathname === '/xrpackage/register') {
+        if (event.request.method === 'POST' && pathname === '/xrpackage/register') {
           return event.request.json()
             .then(j => {
               const {id} = j;
@@ -64,7 +66,6 @@ self.addEventListener('fetch', event => {
             const files = hijackedIds[id];
             // console.log('hijack file 2', client.url, files);
             if (files) {
-              const pathname = new URL(event.request.url).pathname;
               // console.log('hijack file 2', pathname, files);
               if (!/\/xrpackage\//.test(pathname)) {
                 const file = files.find(f => f.pathname === pathname);
@@ -77,7 +78,11 @@ self.addEventListener('fetch', event => {
           }
         }
       }
-      return fetch(event.request);
+      if (/\/xrpackage\//.test(pathname)) {
+        return fetch('https://xrpackage.org' + pathname);
+      } else {
+        return fetch(event.request);
+      }
     })
   );
 });
