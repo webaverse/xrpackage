84e06940054cba60d1df2ba56c053ff2160e858b
Avaer Kazmer <a@modules.io>
2020-05-14 05:35:05 -0400

    Add edit/run pages separation

---
+++ edit.js
@@ -0,0 +1,171 @@
+import * as THREE from './three.module.js';
+import {XRPackageEngine, XRPackage} from './xrpackage.js';
+import {BufferGeometryUtils} from './BufferGeometryUtils.js';
+import address from 'https://contracts.webaverse.com/address.js';
+import abi from 'https://contracts.webaverse.com/abi.js';
+import {pe, renderer, scene, camera} from './run.js';
+
+const apiHost = `https://ipfs.exokit.org/ipfs`;
+const network = 'rinkeby';
+const infuraApiKey = '4fb939301ec543a0969f3019d74f80c2';
+const rpcUrl = `https://${network}.infura.io/v3/${infuraApiKey}`;
+const web3 = new Web3(new Web3.providers.HttpProvider(rpcUrl));
+// window.web3 = web3;
+const contract = new web3.eth.Contract(abi, address);
+
+const _makeTargetMesh = () => {
+  const targetGeometry = BufferGeometryUtils.mergeBufferGeometries([
+    new THREE.BoxBufferGeometry(0.03, 0.2, 0.03)
+      .applyMatrix4(new THREE.Matrix4().makeTranslation(0, -0.1, 0)),
+    new THREE.BoxBufferGeometry(0.03, 0.2, 0.03)
+      .applyMatrix4(new THREE.Matrix4().makeRotationFromQuaternion(new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0, -1, 0), new THREE.Vector3(0, 0, 1))))
+      .applyMatrix4(new THREE.Matrix4().makeTranslation(0, 0, 0.1)),
+    new THREE.BoxBufferGeometry(0.03, 0.2, 0.03)
+      .applyMatrix4(new THREE.Matrix4().makeRotationFromQuaternion(new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0, -1, 0), new THREE.Vector3(1, 0, 0))))
+      .applyMatrix4(new THREE.Matrix4().makeTranslation(0.1, 0, 0)),
+  ]);
+  const geometry = BufferGeometryUtils.mergeBufferGeometries([
+    targetGeometry.clone()
+      .applyMatrix4(new THREE.Matrix4().makeTranslation(-0.5, 0.5, -0.5)),
+    targetGeometry.clone()
+      .applyMatrix4(new THREE.Matrix4().makeRotationFromQuaternion(new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0, 0, -1), new THREE.Vector3(0, -1, 0))))
+      .applyMatrix4(new THREE.Matrix4().makeTranslation(-0.5, -0.5, -0.5)),
+    targetGeometry.clone()
+      .applyMatrix4(new THREE.Matrix4().makeRotationFromQuaternion(new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0, 1, 0), new THREE.Vector3(0, 0, 1))))
+      .applyMatrix4(new THREE.Matrix4().makeTranslation(-0.5, 0.5, 0.5)),
+    targetGeometry.clone()
+      .applyMatrix4(new THREE.Matrix4().makeRotationFromQuaternion(new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0, 1, 0), new THREE.Vector3(1, 0, 0))))
+      .applyMatrix4(new THREE.Matrix4().makeTranslation(0.5, 0.5, -0.5)),
+    targetGeometry.clone()
+      .applyMatrix4(new THREE.Matrix4().makeRotationFromQuaternion(new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0, 1, 0), new THREE.Vector3(1, 0, 0))))
+      .applyMatrix4(new THREE.Matrix4().makeRotationFromQuaternion(new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0, 1, 0), new THREE.Vector3(0, 0, 1))))
+      .applyMatrix4(new THREE.Matrix4().makeTranslation(0.5, 0.5, 0.5)),
+    targetGeometry.clone()
+      .applyMatrix4(new THREE.Matrix4().makeRotationFromQuaternion(new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0, 1, 0), new THREE.Vector3(0, 0, 1))))
+      .applyMatrix4(new THREE.Matrix4().makeRotationFromQuaternion(new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(-1, 0, 0), new THREE.Vector3(0, -1, 0))))
+      .applyMatrix4(new THREE.Matrix4().makeTranslation(-0.5, -0.5, 0.5)),
+    targetGeometry.clone()
+      .applyMatrix4(new THREE.Matrix4().makeRotationFromQuaternion(new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0, 1, 0), new THREE.Vector3(1, 0, 0))))
+      .applyMatrix4(new THREE.Matrix4().makeRotationFromQuaternion(new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(1, 0, 0), new THREE.Vector3(0, -1, 0))))
+      .applyMatrix4(new THREE.Matrix4().makeTranslation(0.5, -0.5, -0.5)),
+    targetGeometry.clone()
+      .applyMatrix4(new THREE.Matrix4().makeRotationFromQuaternion(new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(-1, 1, 0).normalize(), new THREE.Vector3(1, -1, 0).normalize())))
+      .applyMatrix4(new THREE.Matrix4().makeTranslation(0.5, -0.5, 0.5)),
+  ]).applyMatrix4(new THREE.Matrix4().makeTranslation(0, 0.5, 0));
+  const targetVsh = `
+    #define M_PI 3.1415926535897932384626433832795
+    uniform float uTime;
+    varying vec2 vUv;
+    void main() {
+      float f = 1.0 + pow(sin(uTime * M_PI), 0.5) * 0.2;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position * f, 1.);
+    }
+  `;
+  const targetFsh = `
+    uniform float uHighlight;
+    uniform float uTime;
+    void main() {
+      float f = max(1.0 - pow(uTime, 0.5), 0.1);
+      gl_FragColor = vec4(vec3(f * uHighlight), 1.0);
+    }
+  `;
+  const material = new THREE.ShaderMaterial({
+    uniforms: {
+      uHighlight: {
+        type: 'f',
+        value: 0,
+      },
+      uTime: {
+        type: 'f',
+        value: 0,
+      },
+    },
+    vertexShader: targetVsh,
+    fragmentShader: targetFsh,
+    // transparent: true,
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.frustumCulled = false;
+  return mesh;
+};
+/* const targetMesh = _makeTargetMesh();
+scene.add(targetMesh); */
+
+// const objects = [];
+function animate(timestamp, frame) {
+  /* const timeFactor = 1000;
+  targetMesh.material.uniforms.uTime.value = (Date.now() % timeFactor) / timeFactor; */
+
+  renderer.render(scene, camera);
+}
+renderer.setAnimationLoop(animate);
+renderer.xr.setSession(pe.fakeSession);
+
+const _handleUpload = async file => {
+  const d = await XRPackage.compileFromFile(file);
+  const p = new XRPackage(d);
+  await pe.add(p);
+
+  if (/\.vrm$/.test(file.name)) {
+    p.wearAvatar();
+  }
+};
+
+const _bindUploadFileButton = (inputFileEl, handleUpload) => {
+  inputFileEl.addEventListener('change', async e => {
+    const {files} = e.target;
+    if (files.length === 1) {
+      const [file] = files;
+      handleUpload(file);
+    }
+
+    const {parentNode} = inputFileEl;
+    parentNode.removeChild(inputFileEl);
+    const newInputFileEl = inputFileEl.ownerDocument.createElement('input');
+    newInputFileEl.type = 'file';
+    // newInputFileEl.id = 'upload-file-button';
+    // newInputFileEl.style.display = 'none';
+    newInputFileEl.classList.add('hidden');
+    parentNode.appendChild(newInputFileEl);
+    _bindUploadFileButton(newInputFileEl);
+  });
+};
+_bindUploadFileButton(document.getElementById('load-package-input'), file => {
+  window.dispatchEvent(new MessageEvent('upload', {
+    data: file,
+  }));
+});
+
+document.getElementById('shield-slider').addEventListener('change', e => {
+  const shieldLevel = parseInt(e.target.value, 10);
+  const {packages} = pe;
+  switch (shieldLevel) {
+    case 0: {
+      for (const p of packages) {
+        p.visible = false;
+        if (!p.placeholderBox) {
+          p.placeholderBox = _makeTargetMesh();
+          p.placeholderBox.matrix.copy(p.matrix).decompose(p.placeholderBox.position, p.placeholderBox.quaternion, p.placeholderBox.scale);
+        }
+        scene.add(p.placeholderBox);
+      }
+      break;
+    }
+    case 1: {
+      for (const p of packages) {
+        p.visible = true;
+        if (p.placeholderBox) {
+          scene.remove(p.placeholderBox);
+        }
+      }
+      break;
+    }
+  }
+});
+
+const dropdownButton = document.getElementById('dropdown-button');
+const dropdown = document.getElementById('dropdown');
+dropdownButton.addEventListener('click', e => {
+  dropdownButton.classList.toggle('open');
+  dropdown.classList.toggle('open');
+});
---
+++ run.js
@@ -0,0 +1,276 @@
+import * as THREE from './three.module.js';
+import {XRPackageEngine, XRPackage} from './xrpackage.js';
+
+function parseQuery(queryString) {
+  var query = {};
+  var pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
+  for (var i = 0; i < pairs.length; i++) {
+    var pair = pairs[i].split('=');
+    query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+  }
+  return query;
+}
+
+let currentSession = null;
+
+const pe = new XRPackageEngine({
+  orbitControls: true,
+});
+
+/* const canvas = document.createElement('canvas');
+const context = canvas.getContext('webgl', {
+  antialias: true,
+  alpha: true,
+  preserveDrawingBuffer: false,
+}); */
+const renderer = new THREE.WebGLRenderer({
+  canvas: pe.domElement,
+  context: pe.getContext('webgl'),
+  antialias: true,
+  alpha: true,
+  // preserveDrawingBuffer: true,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+// renderer.autoClear = false;
+renderer.sortObjects = false;
+renderer.physicallyCorrectLights = true;
+renderer.xr.enabled = true;
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0.5, 1);
+
+const ambientLight = new THREE.AmbientLight(0xFFFFFF);
+scene.add(ambientLight);
+const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 3);
+scene.add(directionalLight);
+const directionalLight2 = new THREE.DirectionalLight(0xFFFFFF, 3);
+scene.add(directionalLight2);
+
+const cubeMesh = (() => {
+  const geometry = new THREE.BoxBufferGeometry(10, 1, 10);
+  const material = new THREE.MeshStandardMaterial({
+    color: 0x666666,
+    // side: THREE.DoubleSide,
+  });
+  const mesh = new THREE.Mesh(geometry, material);  
+  mesh.frustumCulled = false;
+  return mesh;
+})();
+cubeMesh.position.set(0, -1/2, 0);
+// cubeMesh.rotation.order = 'YXZ';
+scene.add(cubeMesh);
+
+function animate(timestamp, frame) {
+  /* const timeFactor = 1000;
+  targetMesh.material.uniforms.uTime.value = (Date.now() % timeFactor) / timeFactor; */
+
+  renderer.render(scene, camera);
+}
+renderer.setAnimationLoop(animate);
+renderer.xr.setSession(pe.fakeSession);
+
+document.addEventListener('dragover', e => {
+  e.preventDefault();
+});
+document.addEventListener('drop', e => {
+  e.preventDefault();
+
+  if (e.dataTransfer.files.length > 0) {
+    const [file] = e.dataTransfer.files;
+    window.dispatchEvent(new MessageEvent('upload', {
+      data: file,
+    }));
+  }
+});
+window.addEventListener('upload', async e => {
+  const file = e.data;
+
+  const d = await XRPackage.compileFromFile(file);
+  const p = new XRPackage(d);
+  await pe.add(p);
+
+  if (/\.vrm$/.test(file.name)) {
+    p.wearAvatar();
+  }
+});
+
+function onSessionStarted(session) {
+  session.addEventListener('end', onSessionEnded);
+  
+  currentSession = session;
+
+  // renderer.xr.setSession(session);
+  pe.setSession(session);
+}
+function onSessionEnded() {
+  currentSession.removeEventListener('end', onSessionEnded);
+
+  currentSession = null;
+
+  // renderer.xr.setSession(null);
+  pe.setSession(null);
+}
+document.getElementById('enter-xr-button').addEventListener('click', e => {
+  e.preventDefault();
+  e.stopPropagation();
+  
+  if (currentSession === null) {
+    navigator.xr.requestSession('immersive-vr', {
+      optionalFeatures: [
+        'local-floor',
+        'bounded-floor',
+      ],
+    }).then(onSessionStarted);
+  } else {
+    currentSession.end();
+  }
+});
+
+(async () => {
+
+{
+  const q = parseQuery(window.location.search);
+  if (q.i) {
+    const metadataHash = await contract.methods.getMetadata(parseInt(q.i, 10), 'hash').call();
+    const metadata = await fetch(`${apiHost}/${metadataHash}`)
+      .then(res => res.json());
+    const {dataHash} = metadata;
+
+    const arrayBuffer = await fetch(`${apiHost}/${dataHash}.wbn`)
+      .then(res => res.arrayBuffer());
+
+    const p = new XRPackage(new Uint8Array(arrayBuffer));
+    pe.add(p);
+  } else if (q.u) {
+    const arrayBuffer = await fetch(q.u)
+      .then(res => res.arrayBuffer());
+
+    const p = new XRPackage(new Uint8Array(arrayBuffer));
+    pe.add(p);
+  } else if (q.h) {
+    (async () => {
+      const [cubeHtml, cubeManifest] = await Promise.all([
+        (async () => {
+          const res = await fetch('examples/html/cube.html');
+          return await res.text();
+        })(),
+        (async () => {
+          const res = await fetch('examples/html/manifest.json');
+          return await res.text();
+        })(),
+      ]);
+
+      const d = XRPackage.compileRaw(
+        [
+          {
+            url: '/cube.html',
+            type: 'text/html',
+            data: cubeHtml,
+          },
+          {
+            url: '/manifest.json',
+            type: 'application/json',
+            data: cubeManifest,
+          }
+        ]
+      );
+      const p = new XRPackage(d);
+      pe.add(p);
+    })();
+  } else {
+    (async () => {
+      const [cubeHtml, cubeManifest, modelVrm, catVox]= await Promise.all([
+        (async () => {
+          const res = await fetch('examples/html/cube.html');
+          return await res.text();
+        })(),
+        (async () => {
+          const res = await fetch('examples/html/manifest.json');
+          return await res.text();
+        })(),
+        (async () => {
+          const res = await fetch('examples/vrm/model.vrm');
+          return await res.arrayBuffer();
+        })(),
+        (async () => {
+          const res = await fetch('examples/vox/cat.vox');
+          return await res.arrayBuffer();
+        })(),
+      ]);
+
+      {
+        const d = XRPackage.compileRaw(
+          [
+            {
+              url: '/cube.html',
+              type: 'text/html',
+              data: cubeHtml,
+            },
+            {
+              url: '/manifest.json',
+              type: 'application/json',
+              data: cubeManifest,
+            }
+          ]
+        );
+        const p = new XRPackage(d);
+        pe.add(p);
+      }
+      {
+        const d = XRPackage.compileRaw(
+          [
+            {
+              url: '/model.vrm',
+              type: 'application/octet-stream',
+              data: new Uint8Array(modelVrm),
+            },
+            {
+              url: '/manifest.json',
+              type: 'application/json',
+              data: JSON.stringify({
+                xr_type: 'vrm@0.0.1',
+                start_url: 'model.vrm',
+              }),
+            }
+          ]
+        );
+        const p = new XRPackage(d);
+        pe.add(p);
+      }
+      {
+        const d = XRPackage.compileRaw(
+          [
+            {
+              url: '/cat.vox',
+              type: 'application/octet-stream',
+              data: new Uint8Array(catVox),
+            },
+            {
+              url: '/manifest.json',
+              type: 'application/json',
+              data: JSON.stringify({
+                xr_type: 'vox@0.0.1',
+                start_url: 'cat.vox',
+              }),
+            }
+          ]
+        );
+        const p = new XRPackage(d);
+        pe.add(p);
+      }
+    })();
+  }
+}
+
+})();
+
+export {
+  pe,
+  renderer,
+  scene,
+  camera,
+};
