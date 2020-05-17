1001f27a6bab1111be7fb2bc57602baea1f0e11f
Avaer Kazmer <a@modules.io>
2020-05-16 20:29:16 -0400

    Add YXZ rotation order for main camera

--- xrpackage.js
+++ xrpackage.js
@@ -339,6 +339,7 @@ export class XRPackageEngine extends EventTarget {
 
     const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.set(0, 0, 1);
+    camera.rotation.order = 'YXZ';
     this.camera = camera;
 
     const orbitControls = new OrbitControls(camera, canvas, document);
