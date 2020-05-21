8310ba2cfeccaaf9abefc73f68f3e0ffc8f5f4ac
Avaer Kazmer <a@modules.io>
2020-05-21 17:25:50 -0400

    Clean up initial camera position

--- xrpackage.js
+++ xrpackage.js
@@ -380,7 +380,7 @@ export class XRPackageEngine extends EventTarget {
     this.scene = scene;
 
     const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(0, 0, 1);
+    camera.position.set(0, 1, 2);
     camera.rotation.order = 'YXZ';
     this.camera = camera;
 
