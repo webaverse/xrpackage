45e5c3281a291a44235975ebf92a1a9727069d33
Avaer Kazmer <a@modules.io>
2020-05-21 21:48:27 -0400

    Code cleanup

--- run.js
+++ run.js
@@ -1,16 +1,6 @@
 import * as THREE from './three.module.js';
 import {XRPackageEngine, XRPackage} from './xrpackage.js';
 
-function parseQuery(queryString) {
-  var query = {};
-  var pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
-  for (var i = 0; i < pairs.length; i++) {
-    var pair = pairs[i].split('=');
-    query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
-  }
-  return query;
-}
-
 let currentSession = null;
 
 const pe = new XRPackageEngine({
