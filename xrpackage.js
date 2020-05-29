a63b96835a4bae654eb0a60412ef3d99226003ef
Avaer Kazmer <a@modules.io>
2020-05-29 04:44:11 -0400

    Small XR globals cleanup

--- xrpackage.js
+++ xrpackage.js
@@ -432,42 +432,6 @@ export class XRPackageEngine extends EventTarget {
     this.fakeSession.onrequestanimationframe = fn => this.packageRequestAnimationFrame(fn, globalThis);
     this.fakeSession.oncancelanimationframe = this.packageCancelAnimationFrame.bind(this);
 
-    window.OldXR = {
-      XR: window.XR,
-      XRSession: window.XRSession,
-      XRRenderState: window.XRRenderState,
-      XRWebGLLayer: window.XRWebGLLayer,
-      XRFrame: window.XRFrame,
-      XRView: window.XRView,
-      XRViewport: window.XRViewport,
-      XRPose: window.XRPose,
-      XRViewerPose: window.XRViewerPose,
-      XRInputSource: window.XRInputSource,
-      // XRRay,
-      // XRInputPose,
-      XRInputSourceEvent: window.XRInputSourceEvent,
-      XRSpace: window.XRSpace,
-      XRReferenceSpace: window.XRReferenceSpace,
-      XRBoundedReferenceSpace: window.XRBoundedReferenceSpace,
-    };
-
-    window.XR = XR.XR;
-    window.XRSession = XR.XRSession;
-    window.XRRenderState = XR.XRRenderState;
-    window.XRWebGLLayer = XR.XRWebGLLayer;
-    window.XRFrame = XR.XRFrame;
-    window.XRView = XR.XRView;
-    window.XRViewport = XR.XRViewport;
-    window.XRPose = XR.XRPose;
-    window.XRViewerPose = XR.XRViewerPose;
-    window.XRInputSource = XR.XRInputSource;
-    window.XRRay = XR.XRRay;
-    // window.XRInputPose = XR.XRInputPose;
-    window.XRInputSourceEvent = XR.XRInputSourceEvent;
-    window.XRSpace = XR.XRSpace;
-    window.XRReferenceSpace = XR.XRReferenceSpace;
-    window.XRBoundedReferenceSpace = XR.XRBoundedReferenceSpace;
-
     renderer.xr.setSession(this.fakeSession);
 
     this.name = 'XRPackage Scene';
@@ -1549,4 +1513,38 @@ export class XRPackage extends EventTarget {
   }
 }
 
-// window.XRPackage = XRPackage;
+window.OldXR = {
+  XR: window.XR,
+  XRSession: window.XRSession,
+  XRRenderState: window.XRRenderState,
+  XRWebGLLayer: window.XRWebGLLayer,
+  XRFrame: window.XRFrame,
+  XRView: window.XRView,
+  XRViewport: window.XRViewport,
+  XRPose: window.XRPose,
+  XRViewerPose: window.XRViewerPose,
+  XRInputSource: window.XRInputSource,
+  // XRRay,
+  // XRInputPose,
+  XRInputSourceEvent: window.XRInputSourceEvent,
+  XRSpace: window.XRSpace,
+  XRReferenceSpace: window.XRReferenceSpace,
+  XRBoundedReferenceSpace: window.XRBoundedReferenceSpace,
+};
+
+window.XR = XR.XR;
+window.XRSession = XR.XRSession;
+window.XRRenderState = XR.XRRenderState;
+window.XRWebGLLayer = XR.XRWebGLLayer;
+window.XRFrame = XR.XRFrame;
+window.XRView = XR.XRView;
+window.XRViewport = XR.XRViewport;
+window.XRPose = XR.XRPose;
+window.XRViewerPose = XR.XRViewerPose;
+window.XRInputSource = XR.XRInputSource;
+window.XRRay = XR.XRRay;
+// window.XRInputPose = XR.XRInputPose;
+window.XRInputSourceEvent = XR.XRInputSourceEvent;
+window.XRSpace = XR.XRSpace;
+window.XRReferenceSpace = XR.XRReferenceSpace;
+window.XRBoundedReferenceSpace = XR.XRBoundedReferenceSpace;
