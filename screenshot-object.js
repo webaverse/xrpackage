60ee9dccc7750850b73c308b3fa11ce6e32b9dfc
Avaer Kazmer <a@modules.io>
2020-05-30 04:40:56 -0400

    Bugfix screenshot imports

--- screenshot-object.js
+++ screenshot-object.js
@@ -1,4 +1,4 @@
-import THREE from './three.module.js';
+import THREE from './xrpackage/three.module.js';
 import {GLTFExporter} from './GLTFExporter.js';
 import './gif.js';
 import screenshot from './screenshot.js';
