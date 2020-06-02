f7cb87893c18e03edea2b6d7de9f805255456b8d
Avaer Kazmer <a@modules.io>
2020-06-02 03:20:43 -0400

    Dead code cleanup

--- edit.js
+++ edit.js
@@ -89,7 +89,7 @@ const targetMeshGeometry = (() => {
 const targetVsh = `
   #define M_PI 3.1415926535897932384626433832795
   uniform float uTime;
-  varying vec2 vUv;
+  // varying vec2 vUv;
   void main() {
     float f = 1.0 + pow(sin(uTime * M_PI), 0.5) * 0.2;
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position * f, 1.);
