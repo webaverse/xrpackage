fcafeb9640e59ff199efbc21f70bb7b89bd1ceea
Shubham Jain <shubham@sjain.dev>
2020-07-23 12:07:29 +0100

    Finish writing & fix screenshot tests

--- tests/loading-screenshot.js
+++ tests/loading-screenshot.js
@@ -1,13 +1,33 @@
+/* global XRPackage */
 const test = require('ava');
+
 const withPage = require('./utils/_withPage');
 const withStaticServer = require('./utils/_withStaticServer');
 
 let server;
-test.before(() => (server = withStaticServer()));
+test.before(t => {
+  server = withStaticServer();
+  t.timeout(5000);
+});
 
 test('load screenshot of baked xrpk', withPage, async (t, page) => {
   await page.goto(process.env.STATIC_URL, { waitFor: 'load' });
-  console.log(await page.content());
+  const response = await page.evaluate(pageFunction, `${process.env.STATIC_URL}/assets/baked-xrpk.wbn`);
+  t.true(response && response.startsWith('<img src="blob:http://localhost:'));
 });
+
+test('load screenshot of unbaked xrpk', withPage, async (t, page) => {
+  await page.goto(process.env.STATIC_URL, { waitFor: 'load' });
+  const response = await page.evaluate(pageFunction, `${process.env.STATIC_URL}/assets/unbaked-xrpk.wbn`);
+  t.falsy(response);
+});
+
+const pageFunction = async path => {
+  const file = await fetch(path).then(res => res.arrayBuffer());
+  const p = new XRPackage(file);
+  await p.waitForLoad();
+  const screenshot = await p.getScreenshotImage();
+  return screenshot ? screenshot.outerHTML : null;
+}
 
 test.after(() => server.close());
