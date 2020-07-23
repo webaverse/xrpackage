68e4bf322083e9101095fad6c3396f0ce0ace08d
Shubham Jain <shubham@sjain.dev>
2020-07-23 12:55:33 +0100

    Remove test timeout

--- tests/loading-screenshot.js
+++ tests/loading-screenshot.js
@@ -5,10 +5,7 @@ const withPage = require('./utils/_withPage');
 const withStaticServer = require('./utils/_withStaticServer');
 
 let server;
-test.before(t => {
-  server = withStaticServer();
-  t.timeout(5000);
-});
+test.before(() => (server = withStaticServer()));
 
 test('load screenshot of baked xrpk', withPage, async (t, page) => {
   await page.goto(process.env.STATIC_URL, { waitFor: 'load' });
