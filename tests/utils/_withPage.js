59fab6081a9837100ee664a4cddcf439254077bf
Shubham Jain <shubham@sjain.dev>
2020-07-24 18:11:58 +0100

    Revert some changes done to test CI

--- tests/utils/_withPage.js
+++ tests/utils/_withPage.js
@@ -2,14 +2,14 @@ const puppeteer = require('puppeteer');
 
 const addXRPackageScript = async (page, port) => {
   // Import local xrpackage.js using this test's express server's port
-  await page.evaluateOnNewDocument(`
-    window.onload = () => {
-      const script = document.createElement('script');
-      script.type = 'module';
-      script.innerText = 'import { XRPackage, XRPackageEngine } from "http://localhost:${port}/xrpackage.js"; window.XRPackage = XRPackage; window.XRPackageEngine = XRPackageEngine;';
-      document.head.appendChild(script);
-    };
-  `);
+  await page.addScriptTag({
+    type: 'module',
+    content: `
+      import { XRPackage, XRPackageEngine } from "http://localhost:${port}/xrpackage.js";
+      window.XRPackage = XRPackage;
+      window.XRPackageEngine = XRPackageEngine;
+    `,
+  });
 };
 
 module.exports = async (t, run) => {
@@ -20,11 +20,11 @@ module.exports = async (t, run) => {
   });
   const page = await browser.newPage();
   page.on('console', consoleObj => console.log(`Page log: "${consoleObj.text()}"`));
-  await addXRPackageScript(page, t.context.port);
 
   try {
     // Wait for no more network requests for at least 500ms before passing onto main test
     await page.goto(t.context.staticUrl, { waitFor: 'networkidle0' });
+    await addXRPackageScript(page, t.context.port);
     await run(t, page);
   } finally {
     await page.close();
