347a728951a13a08733db4f8b4f9191e7521aaeb
YaHeng Su <yaheng.su1@gmail.com>
2020-07-24 16:15:08 -0600

    fix XRPackage not found error but tests timeout error is back

--- tests/utils/_withPage.js
+++ tests/utils/_withPage.js
@@ -1,17 +1,5 @@
 const puppeteer = require('puppeteer');
 
-const addXRPackageScript = async (page, port) => {
-  // Import local xrpackage.js using this test's express server's port
-  await page.addScriptTag({
-    type: 'module',
-    content: `
-      import { XRPackage, XRPackageEngine } from "http://localhost:${port}/xrpackage.js";
-      window.XRPackage = XRPackage;
-      window.XRPackageEngine = XRPackageEngine;
-    `,
-  });
-};
-
 module.exports = async (t, run) => {
   const browser = await puppeteer.launch({
     headless: true,
@@ -24,7 +12,6 @@ module.exports = async (t, run) => {
   try {
     // Wait for no more network requests for at least 500ms before passing onto main test
     await page.goto(t.context.staticUrl, { waitFor: 'networkidle0' });
-    await addXRPackageScript(page, t.context.port);
     await run(t, page);
   } finally {
     await page.close();
