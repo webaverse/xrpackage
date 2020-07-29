e19f6ba623b8f5fe1004d3301b138df506af699a
Shubham Jain <shubham@sjain.dev>
2020-07-29 14:52:29 +0100

    Add parsing wbn package tests

---
+++ tests/parsing-xrpks.js
@@ -0,0 +1,33 @@
+/* global XRPackage */
+const test = require('ava');
+
+const withPageAndStaticServer = require('./utils/_withPageAndStaticServer');
+
+test('parse webxr-site@0.0.1 package', withPageAndStaticServer, async (t, page) => {
+  const response = await page.evaluate(pageFunction, `${t.context.staticUrl}/assets/webxr-template.wbn`);
+  t.true(response);
+});
+
+test('parse gltf@0.0.1 package', withPageAndStaticServer, async (t, page) => {
+  const response = await page.evaluate(pageFunction, `${t.context.staticUrl}/assets/baked-xrpk.wbn`);
+  t.true(response);
+});
+
+test('parse vrm@0.0.1 package', withPageAndStaticServer, async (t, page) => {
+  const response = await page.evaluate(pageFunction, `${t.context.staticUrl}/assets/avatar.wbn`);
+  t.true(response);
+});
+
+test.todo('parse vox@0.0.1 package');
+
+// Only if we're keeping the world concept in XRPackage
+test.todo('parse xrpackage-scene@0.0.1 package');
+
+const pageFunction = async path => {
+  const file = await fetch(path).then(res => res.arrayBuffer());
+  const p = new XRPackage(file);
+
+  // This will block until the package's loader has run and the load event has been fired
+  await p.waitForLoad();
+  return true;
+};
