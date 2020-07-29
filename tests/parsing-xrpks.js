caad4779d50894d74ab7b50c410a175a8f6b6897
Shubham Jain <shubham@sjain.dev>
2020-07-29 14:56:53 +0100

    Cleanup test file; Reduce repetition

--- tests/parsing-xrpks.js
+++ tests/parsing-xrpks.js
@@ -4,28 +4,29 @@ const test = require('ava');
 const withPageAndStaticServer = require('./utils/_withPageAndStaticServer');
 
 test('parse webxr-site@0.0.1 package', withPageAndStaticServer, async (t, page) => {
-  const response = await page.evaluate(pageFunction, `${t.context.staticUrl}/assets/webxr-template.wbn`);
-  t.true(response);
+  await performTest(t, page, 'assets/webxr-template.wbn');
 });
 
 test('parse gltf@0.0.1 package', withPageAndStaticServer, async (t, page) => {
-  const response = await page.evaluate(pageFunction, `${t.context.staticUrl}/assets/baked-xrpk.wbn`);
-  t.true(response);
+  await performTest(t, page, 'assets/baked-xrpk.wbn');
 });
 
 test('parse vrm@0.0.1 package', withPageAndStaticServer, async (t, page) => {
-  const response = await page.evaluate(pageFunction, `${t.context.staticUrl}/assets/avatar.wbn`);
-  t.true(response);
+  await performTest(t, page, 'assets/avatar.wbn');
 });
 
 test.only('parse vox@0.0.1 package', withPageAndStaticServer, async (t, page) => {
   // Source .vox: https://github.com/mikelovesrobots/mmmm/blob/master/vox/alien_bot1.vox
-  const response = await page.evaluate(pageFunction, `${t.context.staticUrl}/assets/alien_bot1_vox.wbn`);
-  t.true(response);
+  await performTest(t, page, 'assets/alien_bot1_vox.wbn');
 });
 
 // Only if we're keeping the world concept in XRPackage
 test.todo('parse xrpackage-scene@0.0.1 package');
+
+const performTest = async (t, page, path) => {
+  const response = await page.evaluate(pageFunction, `${t.context.staticUrl}/${path}`);
+  t.true(response);
+};
 
 const pageFunction = async path => {
   const file = await fetch(path).then(res => res.arrayBuffer());
