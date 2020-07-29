32b3257f69e942ba693a5ef19b5f79069d73aea1
YaHeng Su <yaheng.su1@gmail.com>
2020-07-29 13:28:18 -0600

    initial stub for test

---
+++ tests/download-upload-package.js
@@ -0,0 +1,11 @@
+const test = require('ava');
+
+const withPageAndStaticServer = require('./utils/_withPageAndStaticServer');
+
+test('download package programmatically', withPageAndStaticServer, async (t, page) => {
+
+});
+
+test('upload package programmatically', withPageAndStaticServer, async (t, page) => {
+
+});
