5860ac411e9da8fa4798efb32f32d28542273067
Shubham Jain <shubham@sjain.dev>
2020-07-31 14:12:14 +0100

    Add test for parsing wbn with no manifest.json

--- tests/bad-data.js
+++ tests/bad-data.js
@@ -22,3 +22,22 @@ test('compiling unknown file type', withPageAndStaticServer, async (t, page) => 
 
   t.true(result);
 });
+
+test('parsing wbn with no manifest', withPageAndStaticServer, async (t, page) => {
+  const result = await page.evaluate(async path => {
+    try {
+      const uint8Array = await fetch(path).then(res => res.arrayBuffer());
+      await new XRPackage(uint8Array);
+    } catch (err) {
+      if (err.message === 'no manifest.json in pack') {
+        return true;
+      } else {
+        console.error('Unexpected error', err.message);
+      }
+    }
+
+    return false;
+  }, `${t.context.staticUrl}/assets/no-manifest.wbn`);
+
+  t.true(result);
+});
